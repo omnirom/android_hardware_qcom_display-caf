c9ce13e7f38d8290810de3358a311f882263742b
Naseer Ahmed <naseer@codeaurora.org>
2013-06-25 17:17:04 -0400

    display-caf: Squashed commits to support devices with older video blobs
    This should correct the retire fence, dequeue buffer, and mdp errors seen on htc s4 devices
    
    Path set 2: cleaned up duplicate ifdefs in common.mk
    
    hwc: Get the retire fence from the MDP driver
    
    Get the retire fence separately instead of duplicating the
    release fence.
    
    Bug: 9319776
    Change-Id: Iacb773c2d94f635e27a7b355d399cb2e36dac2a5
    Signed-off-by: Iliyan Malchev <malchev@google.com>
    
    Conflicts:
    	common.mk
    
    display: Add flag to allow legacy allocations from MM heap
    
     * New devices with IOMMU use the MM heap exclusively for protected
       buffers. We need to support the old behavior for devices with legacy
       userspace.
     * Enable the old behavior with TARGET_DISPLAY_INSECURE_MM_HEAP.
    
    Change-Id: I7882354fa8f159c707be11d7021c18561abfb522
    
    Conflicts:
    	common.mk
    
    Change-Id: Iacb773c2d94f635e27a7b355d399cb2e36dac2a5

--- common.mk
+++ common.mk
@@ -44,15 +44,24 @@ kernel_includes :=
 
 # Executed only on QCOM BSPs
 ifeq ($(TARGET_USES_QCOM_BSP),true)
-# This flag is used to compile out any features that depend on framework changes
+    # This flag is used to compile out any features that depend on framework changes
     common_flags += -DQCOM_BSP
 endif
+
 ifeq ($(call is-vendor-board-platform,QCOM),true)
-# This check is to pick the kernel headers from the right location.
-# If the macro above is defined, we make the assumption that we have the kernel
-# available in the build tree.
-# If the macro is not present, the headers are picked from hardware/qcom/msmXXXX
-# failing which, they are picked from bionic.
+    # This check is to pick the kernel headers from the right location.
+    # If the macro above is defined, we make the assumption that we have the kernel
+    # available in the build tree.
+    # If the macro is not present, the headers are picked from hardware/qcom/msmXXXX
+    # failing which, they are picked from bionic.
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+endif
+
+ifneq ($(TARGET_DISPLAY_INSECURE_MM_HEAP),true)
+    common_flags += -DSECURE_MM_HEAP
+endif
+
+ifeq ($(TARGET_DISPLAY_USE_RETIRE_FENCE),true)
+    common_flags += -DUSE_RETIRE_FENCE
 endif
