7ce390ddd85a0f51b33aa50b68f844472dc2c17e
Lalit Maganti <lalitmaganti@gmail.com>
2013-12-12 16:44:16 +0100

    display-caf: fix mismerge
    Change-Id: Idff24bb576f8ef081231dae8817bce4278b90b1e

--- Android.mk
+++ Android.mk
@@ -6,6 +6,7 @@ display-hals += liblight
 endif
 display-hals += libmemtrack
 
+ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
 ifneq ($(filter msm8226 msm8x26 msm8960 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
