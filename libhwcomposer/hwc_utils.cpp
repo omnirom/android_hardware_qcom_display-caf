47737d0c066f424eae49d6457da8d25d06052e02
Jon Haus <haus.xda@gmail.com>
2014-02-02 18:57:13 +0100

    Revert "display-caf: Squashed commits to support devices with JB video blobs"
    retire fence should be fixed in the kernel
    
    insecure mem heap will need to be replaced
    
    This reverts commit ac0bb2ff7838794c33374cd80332b3ff0059e40e.
    
    Change-Id: I223dbf233ad44d1b359654227f632a2934a812b1

--- libhwcomposer/hwc_utils.cpp
+++ libhwcomposer/hwc_utils.cpp
@@ -755,9 +755,6 @@ int hwc_sync(hwc_context_t *ctx, hwc_display_contents_1_t* list, int dpy,
     int acquireFd[MAX_NUM_APP_LAYERS];
     int count = 0;
     int releaseFd = -1;
-#ifdef USE_RETIRE_FENCE
-    int retireFd = -1;
-#endif
     int fbFd = -1;
     int rotFd = -1;
     bool swapzero = false;
@@ -771,9 +768,6 @@ int hwc_sync(hwc_context_t *ctx, hwc_display_contents_1_t* list, int dpy,
     }
     data.acq_fen_fd = acquireFd;
     data.rel_fen_fd = &releaseFd;
-#ifdef USE_RETIRE_FENCE
-    data.retire_fen_fd = &retireFd;
-#endif
 
     char property[PROPERTY_VALUE_MAX];
     if(property_get("debug.egl.swapinterval", property, "1") > 0) {
@@ -895,20 +889,13 @@ int hwc_sync(hwc_context_t *ctx, hwc_display_contents_1_t* list, int dpy,
         releaseFd = -1;
     }
 
-#ifdef USE_RETIRE_FENCE
-    close(releaseFd);
-    if(UNLIKELY(swapzero))
-        list->retireFenceFd = -1;
-    else
-        list->retireFenceFd = retireFd;
-#else
-    if(UNLIKELY(swapzero)) {
+    if(UNLIKELY(swapzero)){
         list->retireFenceFd = -1;
         close(releaseFd);
     } else {
         list->retireFenceFd = releaseFd;
     }
-#endif
+
     return ret;
 }
 
