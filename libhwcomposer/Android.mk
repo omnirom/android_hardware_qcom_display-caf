77953d8cdd8e657896fd85b946e31f8642360f4e
Łukasz Domeradzki <JustArchi@JustArchi.net>
2014-10-05 00:30:34 +0200

    qcom_display-caf: [2/4] [EXPERIMENTAL] JustArchi's android optimizations
    Change-Id: Ic16c986026bc5a7290d62adb04a219034f436485

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -12,7 +12,7 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
                                  libexternal libqdutils libhardware_legacy \
                                  libdl libmemalloc libqservice libsync \
                                  libbinder libmedia libvirtual libskia
-LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\"
+LOCAL_CFLAGS                  := $(common_flags) -Os -DLOG_TAG=\"qdhwcomposer\"
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := hwc.cpp          \
                                  hwc_utils.cpp    \
